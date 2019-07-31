7729572b41e9938c7e86d8740be905f42bf9a120
leaf4761 <hao-bupt@163.com>
2019-07-31 23:14:28 +0800

    init

---
+++ java/com/ch/algorithms/array_05/GenericArray.java
@@ -0,0 +1,14 @@
+package com.ch.algorithms.array_05;
+
+public class GenericArray<T> {
+    private T[] data;
+    private int size;
+
+    /**
+     * 构造方法，根据传入值构造初始大小
+     * @param capacity
+     */
+    public GenericArray(int capacity){
+        
+    }
+}
